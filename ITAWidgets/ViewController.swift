7f8f8090e2d946ffc36a9f37da1742a6a2138478
Alfredo Cano <alfrecanodiaz@gmail.com>
2019-09-17 10:41:32 -0400

    Initial Commit

---
+++ ITAWidgets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITAWidgets
+//
+//  Created by Alfredo on 9/17/19.
+//  Copyright © 2019 Itaú. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
